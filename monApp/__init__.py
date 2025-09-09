05a69d0cc3390638130c794dd9d8612f161917db
Rousseau Esteban <o22400319@or-iut-i207-f27.iut45.univ-orleans.fr>
2025-09-09 17:00:07 +0200

    fin du tp 2

--- monApp/__init__.py
+++ monApp/__init__.py
@@ -1,2 +1,6 @@
+from .app import app, db
+import monApp.views
+import monApp.commands
+import monApp.models
 from .app import app
 import monApp.views
--- monApp/app.py
+++ monApp/app.py
@@ -8,4 +8,4 @@ app.config.from_object('config')
 # Create database connection object
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
-db.init_app(app)
+#db.init_app(app)
