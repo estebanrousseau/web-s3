db2cd04babc19032d75f22351717fcc3b908918c
Rousseau Esteban <o22400319@or-iut-i207-f26.iut45.univ-orleans.fr>
2025-09-16 16:38:15 +0200

    tp ln(exp(2 * 2 + 7 - 8)) terminé

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import random, string, os
 
 SECRET_KEY = "2lzUl{$*D6#`8uXqlU."
 ABOUT = "Bienvenue sur la page à propos de Flask !" 
-CONTACT = "dhdgyhefgefyguegyfegyifegyrfyi"
+CONTACT = "06 24 67 98 65"
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'monApp.db')
--- monApp/views.py
+++ monApp/views.py
@@ -1,17 +1,18 @@
 from .app import app
+from flask import render_template
 
 @app.route('/about/')
 def about():
-    return app.config['ABOUT']
-
-
-@app.route('/')
-def index():
-    return "Hello world !"
+    return render_template("about.html",title ="R3.01 Dev Web avec Flask",name= app.config['ABOUT']) 
 
 @app.route('/contact/')  
 def contact(): 
-    return app.config['CONTACT']
+    return render_template("contact.html",title ="R3.01 Dev Web avec Flask",name= app.config['CONTACT']) 
+
+@app.route('/')
+@app.route('/index/')
+def index():
+    return render_template("index.html",title ="R3.01 Dev Web avec Flask",name="Cricri")    
 
 if __name__ == "__main__":
     app.run()
