59c04a532e1100bf9555435d22a77e5fe35620ed
Rousseau Esteban <o22400319@or-iut-i207-f06.iut45.univ-orleans.fr>
2025-09-23 16:59:29 +0200

    avancement du tp 4

--- monApp/views.py
+++ monApp/views.py
@@ -1,5 +1,7 @@
 from .app import app
-from flask import render_template
+from flask import render_template, request
+from monApp.models import Auteur
+from monApp.models import Livre
 
 @app.route('/about/')
 def about():
@@ -12,7 +14,29 @@ def contact():
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template("index.html",title ="R3.01 Dev Web avec Flask",name="Cricri")    
+# si pas de paramètres
+    if len(request.args)==0:
+        return render_template("index.html",title="R3.01 Dev Web avec Flask",name="Cricri")
+    else :
+        param_name = request.args.get('name')
+        return render_template("index.html",title="R3.01 Dev Web avec Flask",name=param_name) 
+
+@app.route('/auteurs/')
+def getAuteurs():
+    lesAuteurs = Auteur.query.all()
+    return render_template('auteurs_list.html', title="R3.01 Dev Web avec Flask", auteurs=lesAuteurs)
+    
+
+@app.route('/livres/')
+def getLivres():
+    lesLivres = Livre.query.all()
+    return render_template('livre_list.html', title="R3.01 Dev Web avec Flask", livres=lesLivres)
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run()
